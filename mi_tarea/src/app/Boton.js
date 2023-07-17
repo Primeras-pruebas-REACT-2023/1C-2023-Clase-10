3ea9baed754bbb3f92a72132efb38d9d05b26f11
Walter Mancuso <waltermancuso@abc.gob.ar>
2023-07-16 23:50:00 -0300

    mas boton

---
+++ mi_tarea/src/app/Boton.js
@@ -0,0 +1,5 @@
+export function Botoon(){
+    return(
+        <button>Este es mi boton</button>
+    )
+}
--- mi_tarea/src/app/page.js
+++ mi_tarea/src/app/page.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Botoon } from './Boton'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       Hola wally
+      <div>
+         <Botoon></Botoon>
+      </div>
     </main>
   )
 }
